cfd8f03006c33d10cb5bdf4cd5ac37616012dd03
Lazarone S. Santana <lazarone@objectdata.com.br>
2018-01-31 18:59:45 -0200

    strings.py

---
+++ python-para-iniciantes/strings.py
@@ -0,0 +1,13 @@
+#coding: utf-8
+
+nome="lazarone santana"
+print(nome)
+
+# vai mostar da string nome apenas os caracteres da posicao 0 até a posicao 8
+print(nome[0:8])
+
+print(nome*3)
+
+print("laza" in nome)
+print("Laza" in nome)
+
