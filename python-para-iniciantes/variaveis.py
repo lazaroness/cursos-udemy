7f83f13f456de77ffb93b29c8f77d2aa4b712d69
Lazarone S. Santana <lazarone@objectdata.com.br>
2018-01-31 18:41:29 -0200

    variaveis.py

---
+++ python-para-iniciantes/variaveis.py
@@ -0,0 +1,22 @@
+valor_hora = 4
+dias = 30
+horas_trabalho = 8
+vencimento_mensal = valor_hora * horas_trabalho * dias
+nome = "lazarone"
+
+print(valor_hora)
+print(dias)
+print(horas_trabalho)
+print(vencimento_mensal)
+print(nome)
+
+numero1 = 11
+numero2 = 10
+resultado = numero1 + numero2
+print(resultado)
+
+print(float(valor_hora))
+
+print(int(10.5))
+
+print(str(valor_hora))
