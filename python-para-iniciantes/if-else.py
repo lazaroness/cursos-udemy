465098c725577fbb6026d870f69b3e40e6ed6d9e
Lazarone S. Santana <lazarone@objectdata.com.br>
2018-02-01 19:05:31 -0200

    if else

---
+++ python-para-iniciantes/if-else.py
@@ -0,0 +1,16 @@
+#coding: utf-8
+
+numero = 1
+
+if(numero == 1):
+    print("numero é igual a 1")
+else:
+    print("numero não é igual a 1")
+
+nome = "lazarone"
+if("t" in nome):
+    print("nome tem a letra t")
+elif("z" in nome):
+    print("nome tem a letra z")
+else:
+    pass
