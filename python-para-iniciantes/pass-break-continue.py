0562b435b15b1fd0ff9e4887fd54b62c0dae069e
Lazarone S. Santana <lazarone@objectdata.com.br>
2018-02-01 19:32:29 -0200

    function pass break continue

---
+++ python-para-iniciantes/pass-break-continue.py
@@ -0,0 +1,20 @@
+#coding: utf-8
+
+numero=20
+while True:
+    numero = numero - 1
+    print(numero)
+    if(numero==2):
+        break
+
+numero=10
+while True:
+    numero = numero - 1
+    if(numero==4):
+        continue
+    print(numero)
+    if(numero==2):
+        break
+
+for x in range(0,5):
+    pass
