6087e5b0d49316dfebfc9656774d27a4a15122bc
Lazarone S. Santana <lazarone@objectdata.com.br>
2018-02-01 18:38:07 -0200

    hash

---
+++ python-para-iniciantes/dicionarios.py
@@ -0,0 +1,13 @@
+#coding: utf-8
+
+# Criando uma hash
+telefones = {"lazarone":11993455945, "lazaroneOld":11960429643}
+print(telefones)
+
+# Adicionando um elemento na hash
+telefones["lucia"] = 11901011100
+print(telefones)
+
+# Removendo um elemento da hash
+del telefones["lazaroneOld"]
+print(telefones)
