5ae0ae4b5749ff0bb9a9ad20f82a607c5d5c24cc
Lazarone S. Santana <lazarone@objectdata.com.br>
2018-02-01 19:12:25 -0200

    for loop

---
+++ python-para-iniciantes/for-loop.py
@@ -0,0 +1,12 @@
+#coding: utf-8
+
+for x in range(0, 5):
+    print("Valor de x e:", x)
+
+nome = "lazarone"
+for letra in nome:
+    print(letra)
+
+lista = ["brasil", 2018, "lazarone"]
+for value in lista:
+    print(value)
