e012b5c61bab58d62a102a002562d572073250de
Lazarone S. Santana <lazarone.info.web@gmail.com>
2018-06-27 20:31:55 -0300

    refactoring do projeto pratico portal de noticias com EJS

--- curso-completo-do-desenvolvedor-nodejs-e-mongodb/app.js
+++ curso-completo-do-desenvolvedor-nodejs-e-mongodb/app.js
@@ -4,12 +4,15 @@ var app     = express();
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res){
-	res.send("<html><body>Portal de noticias</body></html>");
+	res.render("home/index");
 });
 
-app.get('/tecnologia', function(req, res){
-	res.render("secao/tecnologia");
-//	res.send("<html><body>Noticias de Tecnologia</body></html>");
+app.get('/formulario_inclusao_noticia', function(req, res){
+	res.render("admin/form_add_noticia");
+});
+
+app.get('/noticias', function(req, res){
+	res.render("noticias/noticias");
 });
 
 app.listen(3000, function(){
