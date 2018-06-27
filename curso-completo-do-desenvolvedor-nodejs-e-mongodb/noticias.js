a1e45dedfedea89ad15d482cbe331a58b455ae30
Lazarone S. Santana <lazarone.info.web@gmail.com>
2018-06-27 19:51:07 -0300

    Respondendo requisições com base na url requisitada

--- curso-completo-do-desenvolvedor-nodejs-e-mongodb/noticias.js
+++ curso-completo-do-desenvolvedor-nodejs-e-mongodb/noticias.js
@@ -1,7 +1,18 @@
 var http = require('http');
 
 var server = http.createServer(function(req, res){
-	res.end("<html><body>Portal de noticias</body></html>");
+
+	var categoria = req.url;
+
+	if (categoria == '/tecnologia') {
+		res.end("<html><body>Portal de Tecnologia</body></html>");
+	} else if (categoria == '/moda') {
+		res.end("<html><body>Portal de Moda</body></html>");
+	} else if (categoria == '/beleza') {
+		res.end("<html><body>Portal de Beleza</body></html>");
+	} else {
+		res.end("<html><body>Portal de noticias</body></html>");
+	}
 });
 
 server.listen(3000);
