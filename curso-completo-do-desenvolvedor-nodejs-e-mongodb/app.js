7af1fc6d698fedff56f8bdf31938a9f58079a856
Lazarone S. Santana <lazarone.info.web@gmail.com>
2018-06-27 20:23:59 -0300

    NPM - Instalando o EJS

--- curso-completo-do-desenvolvedor-nodejs-e-mongodb/app.js
+++ curso-completo-do-desenvolvedor-nodejs-e-mongodb/app.js
@@ -1,13 +1,15 @@
 //var app = require('express')();
 var express = require('express');
 var app     = express();
+app.set('view engine', 'ejs');
 
 app.get('/', function(req, res){
 	res.send("<html><body>Portal de noticias</body></html>");
 });
 
 app.get('/tecnologia', function(req, res){
-	res.send("<html><body>Noticias de Tecnologia</body></html>");
+	res.render("secao/tecnologia");
+//	res.send("<html><body>Noticias de Tecnologia</body></html>");
 });
 
 app.listen(3000, function(){
